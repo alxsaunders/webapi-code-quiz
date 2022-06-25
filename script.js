ee2099cedd37588f78c86a9cc91e7b0d8016b728
alxsaunders <alexsaunders242@gmail.com>
2022-06-25 13:20:48 -0400

    set up function for start button

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 var startbutton = document.getElementById('start-btn')
+var questionConEL = document.getElementById('question-container')
+var highscore = document.getElementById('highscore')
+
+startbutton.addEventListener("click", startQuiz)
+
 
 function startQuiz() {
+    console.log('Started')
+    
+    startbutton.classList.add('hide')
+    questionConEL.classList.remove('hide')
+    
+
+
 
 }
 
